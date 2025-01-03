0769d5ab6a358e04919ded3c26f8137f5533cb59
Douglas Herrick <d3herrick@gmail.com>
2025-01-03 09:09:56 -0500

    changed query sheet reset to clearContent() from clear()

--- apps_script/DirectorSheetLib.js
+++ apps_script/DirectorSheetLib.js
@@ -14,7 +14,7 @@
 // @OnlyCurrentDoc
 //
 const deploymentId                     = "14PvqcKWB7ipcH6WytZZS4rMlmap7bnVOnGD30TgD_FIHzojPALwEzXJN";
-const deploymentVersion                = "29";
+const deploymentVersion                = "30";
 const formDataSheetIdRange             = "form_data_spreadsheet_id";
 const formDataSheetRange               = "form_data";
 const plantingDateRange                = "planting_date";
@@ -536,7 +536,7 @@ function listApplicationData_(sheet) {
     newData = queryResults.getSheet().getDataRange().getValues();
   }
   finally {
-    queryResults.clear();
+    queryResults.clearContent();
   }
 
   if (!isApplicationDataEmpty_(newData)) {
